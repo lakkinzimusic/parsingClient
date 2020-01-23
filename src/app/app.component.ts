90d4feb9fbfa647edc2ae1ff43e8b90925f37ad4
Stas <lakkinzimusic@gmail.com>
2020-01-23 08:18:06 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'parsingClient';
+  articles = [];
+  tags = [];
+  columnArticles = [
+    {field: 'header' },
+    {field: 'domen' },
+    {field: 'date'}
+  ];
+
+  columnTags = [
+    {field: 'tag' },
+  ];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:3001').subscribe((response: any) => {
+        // let datad = JSON.parse(data);
+        console.log(response.tags)
+        this.articles = response.articles;
+        this.tags = response.tags;
+      }
+    );
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+
+import {HttpClientModule} from '@angular/common/http';
+import {ClarityModule} from '@clr/angular';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AgGridModule} from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([]),
+    HttpClientModule,
+    ClarityModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
